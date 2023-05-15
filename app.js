fab1a11973d26c36b85dd35c0f9731a6dca0f15e
Ralph Hung <hung5422@tdi.edu.tw>
2023-05-15 11:16:12 +0800

    add method-override package

--- app.js
+++ app.js
@@ -1,16 +1,14 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+const methodOverride = require("method-override");
 const mainRoute = require("./routes/main_route");
 
 app.set("view engine", "ejs");
 
 app.use(express.json());
-app.use(
-  express.urlencoded({
-    extended: true,
-  })
-);
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
 
 app.use("/", mainRoute);
 
