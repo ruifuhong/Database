870948996c0ab16e1c722b0ef255ca345c76f73c
Ralph Hung <hung5422@tdi.edu.tw>
2023-05-15 15:43:17 +0800

    test mysql with relation trying

--- routes/homepage_route.js
+++ routes/homepage_route.js
@@ -4,17 +4,12 @@ const router = express.Router();
 module.exports = (connection) => {
   router.get("/", async (req, res) => {
     try {
-      let cities = connection.query(
-        "SELECT * FROM final.product",
-        (err, products, fields) => {
-          if (err) {
-            return res
-              .status(500)
-              .send("error occurred when searching the data");
-          }
-          return res.render("homepage", { products });
+      connection.query("SELECT * FROM final.trying", (err, items, fields) => {
+        if (err) {
+          return res.status(500).send("error occurred when searching the data");
         }
-      );
+        return res.render("homepage", { items });
+      });
     } catch (e) {
       return res.status(500).send("error occurred when searching the data");
     }
