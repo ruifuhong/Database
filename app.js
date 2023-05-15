7920e8a85bc81432cd63429ef9845164531a78c9
Ralph Hung <hung5422@tdi.edu.tw>
2023-05-15 11:47:58 +0800

    add mysql connection

--- app.js
+++ app.js
@@ -1,14 +1,36 @@
+const mysql = require("mysql");
 const express = require("express");
 const app = express();
 const port = 3000;
 const methodOverride = require("method-override");
 const mainRoute = require("./routes/main_route");
+require("dotenv").config();
 
 app.set("view engine", "ejs");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
+
+const connection = mysql.createConnection({
+  host: process.env.HOST,
+  user: process.env.USER,
+  password: process.env.PASSWORD,
+  database: process.env.DATABASE,
+});
+
+connection.connect((err) => {
+  if (err) {
+    console.log("error");
+    return;
+  }
+  console.log("success");
+});
+
+connection.query("SELECT 15 + 1 as solution", (err, rows, fields) => {
+  if (err) throw err;
+  console.log("The solution is: ", rows[0].solution);
+});
 
 app.use("/", mainRoute);
 
