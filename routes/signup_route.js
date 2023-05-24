5d9c64abc23f67bfaeacc5e08cbd36bccb878052
nccuWUduck <bb222222rrrr@gmail.com>
2023-05-24 18:22:30 +0800

    註冊寫完了
    註冊_將資料insert進SQL
    密碼是以hash的形式

--- routes/signup_route.js
+++ routes/signup_route.js
@@ -11,10 +11,10 @@ module.exports = (connection) => {
   });
 
   router.post("/", async (req, res) => {
+    console.log(req.body)
     const { username, email, lastName, firstName, address, password, confirmPassword } = req.body;
     console.log( username, email, lastName, firstName, address, password, confirmPassword)
     try {
-      console.log( username, email, lastName, firstName, address, password, confirmPassword)
       // 检查密码和确认密码是否匹配
       if (password !== confirmPassword) {
         return res.status(400).json({ message: "密码和确认密码不匹配" });
@@ -23,7 +23,7 @@ module.exports = (connection) => {
       // 生成密码的哈希值
       const hashedPassword = await bcrypt.hash(password, 10);
 
-      const sql = "INSERT INTO final.customer (username, password, First_name, Last_name, registration_date , address) VALUES (?, ?, ?, ?, NOW(), ?)";
+      const sql = "INSERT INTO final.customer (username, password, First_name, Last_name, Joined_since , address) VALUES (?, ?, ?, ?, NOW(), ?)";
       const values = [username, hashedPassword, firstName, lastName, address];
 
       connection.query(sql, values, (error, results) => {
@@ -34,8 +34,8 @@ module.exports = (connection) => {
           res.redirect("/signup");
         } else {
           console.log("注册成功");
-          res.json({ message: "注册成功" });
-          res.redirect("/");
+          //res.json({ message: "注册成功" });
+          res.redirect("/?message=注册成功");
 
         }
       });
