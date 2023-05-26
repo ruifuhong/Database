efc4da1fea2ebcedd28984bb63608c0f396e82f0
bellekao <101861376+bellekao@users.noreply.github.com>
2023-05-26 14:08:46 +0800

    Add files via upload

---
+++ Update_color_size.sql
@@ -0,0 +1,72 @@
+USE FINAL;
+-- SELECT * FROM PRODUCT_COLOR;
+-- SELECT * FROM PRODUCT_SIZE;
+
+INSERT INTO `product_color` VALUES (1,'白色');
+INSERT INTO `product_color` VALUES (1,'淺粉');
+INSERT INTO `product_color` VALUES (1,'淺藍');
+INSERT INTO `product_size` VALUES (1,'S');
+INSERT INTO `product_size` VALUES (1,'M');
+INSERT INTO `product_size` VALUES (1,'L');
+
+INSERT INTO `product_color` VALUES (2,'淺灰');
+INSERT INTO `product_color` VALUES (2,'淺藍');
+INSERT INTO `product_size` VALUES (2,'S');
+INSERT INTO `product_size` VALUES (2,'M');
+INSERT INTO `product_size` VALUES (2,'L');
+
+INSERT INTO `product_color` VALUES (3,'藍色');
+INSERT INTO `product_color` VALUES (3,'淺藍');
+INSERT INTO `product_size` VALUES (3,'S');
+INSERT INTO `product_size` VALUES (3,'M');
+INSERT INTO `product_size` VALUES (3,'L');
+
+INSERT INTO `product_color` VALUES (4,'白色');
+INSERT INTO `product_color` VALUES (4,'黑色');
+INSERT INTO `product_color` VALUES (4,'淺藍');
+INSERT INTO `product_size` VALUES (4,'S');
+INSERT INTO `product_size` VALUES (4,'M');
+INSERT INTO `product_size` VALUES (4,'L');
+
+INSERT INTO `product_color` VALUES (5,'黑色');
+INSERT INTO `product_color` VALUES (5,'藍色');
+INSERT INTO `product_size` VALUES (5,'S');
+INSERT INTO `product_size` VALUES (5,'M');
+INSERT INTO `product_size` VALUES (5,'L');
+
+
+INSERT INTO `product_color` VALUES (6,'白色');
+INSERT INTO `product_color` VALUES (6,'淺粉');
+INSERT INTO `product_color` VALUES (6,'藍色');
+INSERT INTO `product_size` VALUES (6,'S');
+INSERT INTO `product_size` VALUES (6,'M');
+INSERT INTO `product_size` VALUES (6,'L');
+
+INSERT INTO `product_color` VALUES (7,'淺藍');
+INSERT INTO `product_size` VALUES (7,'S');
+INSERT INTO `product_size` VALUES (7,'M');
+INSERT INTO `product_size` VALUES (7,'L');
+
+INSERT INTO `product_color` VALUES (8,'藍色');
+INSERT INTO `product_color` VALUES (8,'淺灰');
+INSERT INTO `product_size` VALUES (8,'S');
+INSERT INTO `product_size` VALUES (8,'M');
+INSERT INTO `product_size` VALUES (8,'L');
+
+INSERT INTO `product_color` VALUES (9,'米白');
+INSERT INTO `product_color` VALUES (9,'淺灰');
+INSERT INTO `product_size` VALUES (9,'S');
+INSERT INTO `product_size` VALUES (9,'M');
+INSERT INTO `product_size` VALUES (9,'L');
+
+INSERT INTO `product_color` VALUES (10,'白色');
+INSERT INTO `product_color` VALUES (10,'淺灰');
+INSERT INTO `product_color` VALUES (10,'黑色');
+INSERT INTO `product_size` VALUES (10,'S');
+INSERT INTO `product_size` VALUES (10,'M');
+INSERT INTO `product_size` VALUES (10,'L');
+
+
+
+
+
