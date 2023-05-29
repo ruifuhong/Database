3c6df3eb96ae6ad4c5638a3465702ce77997de54
nccuWUduck <bb222222rrrr@gmail.com>
2023-05-29 17:40:20 +0800

    try
    試著改些東西

--- page/src/routes/category.jsx
+++ page/src/routes/category.jsx
@@ -63,7 +63,7 @@ const Categories = () => {
             <section className="py-5">
                 <div className="container px-4 px-lg-5 mt-5">
                     <div className="row gx-4 gx-lg-5 row-cols-2 row-cols-md-3 row-cols-xl-4 justify-content-center">
-                        {product.map((item, index) => (
+                        {product.slice(0, 8).map((item, index) => (
                             <div className="col mb-5" key={index}>
                                 <div className="card h-100">
                                     <a
