cf838e376757808868c55d0d8be77f63011eb09f
nccuWUduck <bb222222rrrr@gmail.com>
2023-06-06 16:19:49 +0800

    改位置
    useEffect, useState
    useState, useEffect

--- page/src/routes/member.jsx
+++ page/src/routes/member.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router";
 const baseUrl = process.env.REACT_APP_BASE_URL;
