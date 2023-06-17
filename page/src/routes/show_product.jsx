bb4c0dc81d24f57260cf7f028bb842fec194693c
Hong Rui-Fu (Ralph) <80564528+ruifuhong@users.noreply.github.com>
2023-06-17 15:37:45 +0800

    Welcome to Wu4Shan Shopping Website

--- page/src/routes/show_product.jsx
+++ page/src/routes/show_product.jsx
@@ -131,7 +131,7 @@ const ProductShow = () => {
       <div className="container px-4 px-lg-5 my-5">
         <div className="text-center text-white">
           <h1 className="display-4 fw-bolder">Shop in style</h1>
-          <p className="lead fw-normal text-white-50 mb-0">With this shop hompeage template</p>
+          <p className="lead fw-normal text-white-50 mb-0">Welcome to Wu4Shan Shopping Website</p>
         </div>
       </div>
     </header>
